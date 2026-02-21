01e281e779f74e1fbaceeb27699b2fca133c0f11
Regiane Melo <regianemelo1995@gmail.com>
2026-02-21 17:09:27 -0300

    adicionando rotas

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 import { nanoid } from 'nanoid';
 import connectDB from './config/database.js';
+import auditModel from './models/audit.js';
 import clientModel from './models/client.js';
 import proposalModel from './models/proposal.js';
 dotenv.config();
@@ -43,6 +44,81 @@ app.post("/proposals", async (req, res) => {
     proposal.id = nanoid()
     const newProposals = await proposalModel.create(proposal)
     res.json(newProposals)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+
+
+app.patch("/proposals/:id", async (req, res) => {
+  try {
+    const id = req.params.id
+    const proposals = await proposalModel.find({id})
+    console.log(proposals)
+    res.json(proposals)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+app.post("/proposals/:proposal_id/:status", async (req, res) => {
+  try {
+    console.log(req.params)
+    const proposal_id = req.params.proposal_id
+    const newStatus = req.params.status
+    const proposal = await proposalModel.findOne({id: proposal_id})
+    const audit = {
+      id: nanoid(),
+      proposal_id,
+      actor: "system",
+      event: 'STATUS_CHANGED',
+      payload: JSON.stringify({oldStatus: proposal.status, newStatus})
+    }
+    await auditModel.create(audit)
+    proposal.status = newStatus
+    proposal.save()
+    console.log(proposal)
+    res.json(proposal)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+app.get("/proposals/:id", async (req, res) => {
+  try {
+    const id = req.params.id
+    const proposals = await proposalModel.find({id})
+    console.log(proposals)
+    res.json(proposals)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+app.get("/proposals", async (req, res) => {
+  try {
+    const proposal = req.body
+    const proposals = await proposalModel.find(proposal)
+    console.log(proposals)
+    res.json(proposals)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+app.get("/proposals/:id/audit", async (req, res) => {
+  try {
+    console.log(req.params)
+    const id = req.params.id
+    const proposals = await auditModel.find({proposal_id: id})
+    console.log("asfasfaf", proposals)
+    res.json(proposals)
   } catch (error) {
     console.log('error', error);
     res.json({error})
