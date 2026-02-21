f81e49fa88cf656d339b0694224b00add2205581
Regiane Melo <regianemelo1995@gmail.com>
2026-02-21 14:37:29 -0300

    criando rotas client

---
+++ src/server.js
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { nanoid } from 'nanoid';
+import connectDB from './config/database.js';
+import clientModel from './models/client.js';
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+console.log(PORT)
+
+app.use(express.json())
+
+app.post("/clients", async (req, res) => {
+  try {
+    const client = req.body;
+    client.id = nanoid()
+    const newClient = await clientModel.create(client);
+    res.json(newClient);
+    
+  } catch (error) {
+    console.log('error', error);
+    res.json({error});
+  }
+})
+
+app.get("/clients/:id", async (req, res) => {
+  try {
+    const id = req.params.id
+    const clients = await clientModel.find({id})
+    console.log(clients)
+    res.json(clients)
+  } catch (error) {
+    console.log('error', error);
+    res.json({error})
+  }
+})
+
+connectDB().then(() => {
+  app.listen(PORT, () => {
+  console.log(`O servidor está rodando na porta ${PORT}`);
+  })
+})
